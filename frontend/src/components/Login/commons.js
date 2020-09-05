13d2456fec462a4e14bdc80ce023d089aa988001
erniker <josepablomhotmail.comedinagrande@>
2020-09-05 15:58:49 +0200

    clean file

--- frontend/src/components/Login/commons.js
+++ frontend/src/components/Login/commons.js
@@ -1,28 +1,7 @@
 import { useState, useEffect } from "react";
-import ContactsApi from "../../api/ContactsApi";
-
-const contactsApi = new ContactsApi();
 
 const useLoginForm = (props) => {
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    setLoading(true);
-    try {
-      const user = await login(username, password);
-      dispatch({ type: "LOAD_USER", user });
-      navigate("/");
-    } catch (error) {
-      setLoading(false);
-      if (error.data.statusCode) {
-        const messageType = typeof error.data.message;
-        const message = error.data.message;
-        setErrors({
-          [`${error.data.error}:`]:
-            messageType === "string" ? [message] : message,
-        });
-      }
-    }
-  };
+  const handleSubmit = async (event) => {};
 
   return { handleSubmit };
 };
